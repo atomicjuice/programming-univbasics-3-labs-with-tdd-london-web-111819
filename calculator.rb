e53689581f6f239752690cddc0a8cb4318adc642
keemo <keemowatson1@gmail.com>
2019-11-07 17:19:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@
 first_number = 1
 second_number = 2
 sum =
-(first_number + second_number),
-(first_number - second_number),
-(first_number * second_number),
-(first_number / second_number)
+first_number + second_number,
+first_number - second_number,
+first_number * second_number,
+first_number / second_number
